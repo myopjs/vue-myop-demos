3f9c155f021957bfac63507684d78259d784160e
Віктор Кондоловський <viktor@Viktors-MacBook-Pro.local>
2025-06-23 19:30:57 +0300

    fix styles for skins

--- remote/src/components/Demo2/VueDemo.vue
+++ remote/src/components/Demo2/VueDemo.vue
@@ -27,15 +27,15 @@ const localFunction = () => {
         </div>
       </div>
       <div class="content-text">
-        <h2 class="title">Check out the  vue Component.</h2>
+        <h2 class="title">Check out the &nbsp;vue Component.</h2>
         <p class="subtitle">Push me to production with Myop</p>
       </div>
       <div class="buttons-container">
-        <button class="btn btn-outline" @click="()=> onLikeClicked  ? onLikeClicked() : localFunction()">
+        <button myop-id="button1" class="btn btn-outline" @click="()=> onLikeClicked  ? onLikeClicked() : localFunction()">
           <HeartIcon/>
           Love this
         </button>
-        <button class="btn btn-primary" @click="()=> onCoolClicked  ? onCoolClicked() : localFunction()">
+        <button myop-id="button2" class="btn btn-primary" @click="()=> onCoolClicked  ? onCoolClicked() : localFunction()">
           It’s nice
         </button>
       </div>
